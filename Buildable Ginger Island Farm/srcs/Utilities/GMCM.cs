d99338989e55c76dbe0a3ab5db33ebba3af6bda8
mouahrara <mouahrara@users.noreply.github.com>
2024-01-22 15:21:49 +0100

    Buildable Ginger Island Farm 1.0.0

---
+++ Buildable Ginger Island Farm/srcs/Utilities/GMCM.cs
@@ -0,0 +1,49 @@
+namespace BuildableGingerIslandFarm.Utilities
+{
+	public sealed class ModConfig
+	{
+		public bool	AllowBuildingInSlimeArea = false;
+	}
+
+	internal class GMCMUtility
+	{
+		internal static void Initialize()
+		{
+			ReadConfig();
+			Register();
+		}
+
+		private static void ReadConfig()
+		{
+			ModEntry.Config = ModEntry.Helper.ReadConfig<ModConfig>();
+		}
+
+		private static void Register()
+		{
+			// Get Generic Mod Config Menu's API
+			GenericModConfigMenu.IGenericModConfigMenuApi gmcm = ModEntry.Helper.ModRegistry.GetApi<GenericModConfigMenu.IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+			if (gmcm is null)
+				return;
+
+			// Register mod
+			gmcm.Register(
+				mod: ModEntry.ModManifest,
+				reset: () => ModEntry.Config = new ModConfig(),
+				save: () => ModEntry.Helper.WriteConfig(ModEntry.Config)
+			);
+
+			// Main
+			gmcm.AddBoolOption(
+				mod: ModEntry.ModManifest,
+				name: () => ModEntry.Helper.Translation.Get("GMCM.AllowBuildingInSlimeArea.Title"),
+				tooltip: () => ModEntry.Helper.Translation.Get("GMCM.AllowBuildingInSlimeArea.Tooltip"),
+				getValue: () => ModEntry.Config.AllowBuildingInSlimeArea,
+				setValue: (value) => 
+				{
+					ModEntry.Config.AllowBuildingInSlimeArea = value;
+					GingerIslandFarmUtility.UpdateSlimeArea();
+				}
+			);
+		}
+	}
+}
