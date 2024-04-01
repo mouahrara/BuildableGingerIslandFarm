06690f2c27d98ac113244e27ea0232009143bf12
mouahrara <mouahrara@users.noreply.github.com>
2024-04-01 14:02:14 +0200

    Add compatibility with Lnh's Ginger Island Overhaul

---
+++ Buildable Ginger Island Farm/srcs/Utilities/Compatibility.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildableGingerIslandFarm.Utilities
+{
+	internal class Compatibility
+	{
+		internal static readonly bool IsIslandOverhaulLoaded = ModEntry.Helper.ModRegistry.IsLoaded("Lnh.IslandOverhaul");
+	}
+}
---
+++ Buildable Ginger Island Farm/srcs/Utilities/PointComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace BuildableGingerIslandFarm.Utilities
+{
+	class PointComparer : IEqualityComparer<Point>
+	{
+		public bool Equals(Point x, Point y)
+		{
+			return x.X == y.X && x.Y == y.Y;
+		}
+
+		public int GetHashCode(Point obj)
+		{
+			return (obj.Y << 16) ^ obj.X;
+		}
+	}
+}
