cd86bfc260dcd3b1b81fddd76a9bae919f885b94
mouahrara <mouahrara@users.noreply.github.com>
2024-04-01 14:02:03 +0200

    Buildable Ginger Island Farm 1.0.0

---
+++ Buildable Ginger Island Farm/srcs/Handlers/AssetRequested.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI.Events;
+using BuildableGingerIslandFarm.Utilities;
+
+namespace BuildableGingerIslandFarm.Handlers
+{
+	internal static class AssetRequestedHandler
+	{
+		/// <inheritdoc cref="IContentEvents.AssetRequested"/>
+		/// <param name="sender">The event sender.</param>
+		/// <param name="e">The event data.</param>
+		internal static void Apply(object sender, AssetRequestedEventArgs e)
+		{
+			// Make Ginger Island Farm always active
+			GingerIslandFarmUtility.MakeAlwaysActive(e);
+		}
+	}
+}
---
+++ Buildable Ginger Island Farm/srcs/ModEntry.cs
@@ -0,0 +1,29 @@
+﻿using StardewModdingAPI;
+using BuildableGingerIslandFarm.Handlers;
+using BuildableGingerIslandFarm.Utilities;
+
+namespace BuildableGingerIslandFarm
+{
+	/// <summary>The mod entry point.</summary>
+	internal sealed class ModEntry : Mod
+	{
+		// Shared static helpers
+		internal static new IModHelper	Helper { get; private set; }
+		internal static new IMonitor	Monitor { get; private set; }
+		internal static new IManifest	ModManifest { get; private set; }
+
+		public static ModConfig	Config;
+
+		public override void Entry(IModHelper helper)
+		{
+			Helper = base.Helper;
+			Monitor = base.Monitor;
+			ModManifest = base.ModManifest;
+
+			// Subscribe to events
+			Helper.Events.GameLoop.GameLaunched += GameLaunchedHandler.Apply;
+			Helper.Events.GameLoop.DayStarted += DayStartedHandler.Apply;
+			Helper.Events.Content.AssetRequested += AssetRequestedHandler.Apply;
+		}
+	}
+}
