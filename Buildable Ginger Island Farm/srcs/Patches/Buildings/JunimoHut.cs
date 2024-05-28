2c7acd83fc201ff5cff50b7336daf2d9cca6b6ad
mouahrara <mouahrara@users.noreply.github.com>
2024-05-28 19:32:47 +0200

    Fix Junimos not harvesting until the player visits the Ginger Island farm

---
+++ Buildable Ginger Island Farm/srcs/Patches/Buildings/JunimoHut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HarmonyLib;
+using StardewValley.Buildings;
+
+namespace BuildableGingerIslandFarm.Patches
+{
+	internal class JunimoHutPatch
+	{
+		internal static void Apply(Harmony harmony)
+		{
+			harmony.Patch(
+				original: AccessTools.Method(typeof(JunimoHut), nameof(JunimoHut.dayUpdate), new Type[] { typeof(int) }),
+				postfix: new HarmonyMethod(typeof(JunimoHutPatch), nameof(DayUpdatePostfix))
+			);
+		}
+
+		private static void DayUpdatePostfix(JunimoHut __instance)
+		{
+			__instance.shouldSendOutJunimos.Value = true;
+		}
+	}
+}
