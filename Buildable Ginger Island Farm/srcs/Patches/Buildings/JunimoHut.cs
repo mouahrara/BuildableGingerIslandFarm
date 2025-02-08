bd6995f0f31e5bfc0461f9e4ba04d383afc66da4
mouahrara <mouahrara@users.noreply.github.com>
2025-02-08 22:03:58 +0100

    Fix an issue where raisins from Junimo Huts on Ginger Island were not consumed during winter

--- Buildable Ginger Island Farm/srcs/Patches/Buildings/JunimoHut.cs
+++ Buildable Ginger Island Farm/srcs/Patches/Buildings/JunimoHut.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewValley;
 using StardewValley.Buildings;
 
 namespace BuildableGingerIslandFarm.Patches
@@ -10,8 +17,43 @@ namespace BuildableGingerIslandFarm.Patches
 		{
 			harmony.Patch(
 				original: AccessTools.Method(typeof(JunimoHut), nameof(JunimoHut.dayUpdate), new Type[] { typeof(int) }),
+				transpiler: new HarmonyMethod(typeof(JunimoHutPatch), nameof(Transpiler)),
 				postfix: new HarmonyMethod(typeof(JunimoHutPatch), nameof(DayUpdatePostfix))
 			);
+			harmony.Patch(
+				original: AccessTools.Method(typeof(JunimoHut), nameof(JunimoHut.draw), new Type[] { typeof(SpriteBatch) }),
+				transpiler: new HarmonyMethod(typeof(JunimoHutPatch), nameof(Transpiler))
+			);
+		}
+
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
+		{
+			try
+			{
+				List<CodeInstruction> list = instructions.ToList();
+
+				for (int i = 0; i < list.Count; i++)
+				{
+					if (list[i].opcode.Equals(OpCodes.Call) && list[i].operand.Equals(typeof(Game1).GetProperty(nameof(Game1.IsWinter)).GetGetMethod()))
+					{
+						CodeInstruction[] replacementInstructions = new CodeInstruction[]
+						{
+							new(OpCodes.Ldarg_0) { labels = list[i].labels },
+							new(OpCodes.Callvirt, typeof(Building).GetMethod(nameof(Building.GetParentLocation))),
+							new(OpCodes.Callvirt, typeof(GameLocation).GetMethod(nameof(GameLocation.IsWinterHere)))
+						};
+						list.InsertRange(i, replacementInstructions);
+						i += replacementInstructions.Length;
+						list.RemoveAt(i);
+					}
+				}
+				return list;
+			}
+			catch (Exception e)
+			{
+				ModEntry.Monitor.Log($"There was an issue modifying the instructions for {typeof(JunimoHut)}.{original.Name}: {e}", LogLevel.Error);
+				return instructions;
+			}
 		}
 
 		private static void DayUpdatePostfix(JunimoHut __instance)
